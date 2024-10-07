05430282cd66a9a0f2a643708f0094f987760579
HaarDD <maxgalitskiy@gmail.com>
2024-10-08 02:05:37 +0300

    feat: cp.js task complete

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'node:child_process';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const spawnChildProcess = (args) => {
+    const child = spawn('node', [join(__dirname, 'files', 'script.js'), ...args], {
+        stdio: ['pipe', 'pipe', 'inherit']
+    });
+
+    process.stdin.pipe(child.stdin);
+
+    child.stdout.on('data', (data) => {
+        process.stdout.write(data); 
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
