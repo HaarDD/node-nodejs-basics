502a604211e660c2a2aa2d95fcba935721aaab2e
HaarDD <maxgalitskiy@gmail.com>
2024-10-08 02:04:14 +0300

    feat: read.js task complete

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import { readFile, access } from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePath = join(__dirname, 'files', 'fileToRead.txt');
+    const errorMsg = 'FS operation failed';
+    const successMsgPart = `Content of file \"${filePath}\":\n`;
+
+    try {
+        await access(filePath);
+        const data = await readFile(filePath, 'utf-8');
+        console.log(successMsgPart, data);
+    } catch (error) {
+        throw new Error(errorMsg);
+    }
 };
 
 await read();
