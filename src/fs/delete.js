9e4a2c7d39da8cfc788541efdb46e28c8f6a1776
HaarDD <maxgalitskiy@gmail.com>
2024-10-08 02:02:49 +0300

    feat: delete.js task complete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import { unlink, access } from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const remove = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+    const errorMsg = 'FS operation failed';
+    const successMsg = `File ${filePath} has been removed successfully.`;
+
+    try {
+        await access(filePath);
+        await unlink(filePath);
+        console.log(successMsg);
+    } catch (error) {
+        throw new Error(errorMsg);
+    }
 };
 
 await remove();
