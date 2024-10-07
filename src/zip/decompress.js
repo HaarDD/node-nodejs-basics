d57c67b629a0c46cfb73894e9f2ee47082f8f36c
HaarDD <maxgalitskiy@gmail.com>
2024-10-08 02:04:51 +0300

    feat: decompress.js task complete

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const decompress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const currentPath = join(__dirname, 'files');
+    const filePath = join(currentPath, 'archive.gz');
+    const outputPath = join(currentPath, 'fileToCompress.txt');
+
+    const readStream = createReadStream(filePath);
+    const writeStream = createWriteStream(outputPath);
+    const gunzip = createGunzip();
+
+    const successMsg = `File decompressed successfully to ${outputPath}`;
+
+    readStream.pipe(gunzip).pipe(writeStream);
+    writeStream.on('finish', () => {
+        console.log(successMsg);
+    });
 };
 
 await decompress();
