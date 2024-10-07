c7196228d933d1e508eff0dc33ef9f8be0798fc6
HaarDD <maxgalitskiy@gmail.com>
2024-10-08 02:03:01 +0300

    feat: list.js task complete

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { readdir, access } from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const list = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const dirPath = join(__dirname, 'files');
+    const errorMsg = 'FS operation failed';
+    const successMsgPart = 'Files in directory:';
+
+    try {
+        await access(dirPath);
+        const files = await readdir(dirPath);
+        console.log(successMsgPart, files);
+    } catch (error) {
+        throw new Error(errorMsg);
+    }
 };
 
 await list();
