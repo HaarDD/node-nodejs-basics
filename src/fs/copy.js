218483c0ef8c4a094d2a55ac360f35155fde60a5
HaarDD <maxgalitskiy@gmail.com>
2024-10-08 02:02:07 +0300

    feat: copy.js task complete

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,39 @@
+import { cp, stat } from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const copy = async () => {
-    // Write your code here 
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __pathname = dirname(__filename);
+
+    const sourcePath = join(__pathname, 'files');
+    const destinationPath = join(__pathname, 'files_copy');
+
+    const accessErrors = ['ENOENT', 'ENOTDIR'];
+    const errorMsg = 'FS operation failed';
+    const successMsg = `Directory: ${sourcePath}\nwas succesfully copied in:\n${destinationPath}!`;
+
+    try {
+        await stat(sourcePath);
+    } catch {
+        throw new Error(errorMsg);
+    }
+
+    try {
+        await stat(destinationPath);
+        throw new Error(errorMsg);
+    } catch (error) {
+        console.log(error);
+        if (accessErrors.includes(error.code)) {
+            await cp(sourcePath, destinationPath, { recursive: true });
+            console.log(successMsg);
+        }
+        else {
+            throw error;
+        }
+    }
+
 };
 
 await copy();
