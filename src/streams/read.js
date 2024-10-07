cabefc260c037e750d3f39965e0ae7554e51fa40
HaarDD <maxgalitskiy@gmail.com>
2024-10-08 02:03:06 +0300

    feat: read.js task complete

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,31 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+    const readStream = createReadStream(filePath, { encoding: 'utf8' });
+
+    const errorMsgPart = 'Error: ';
+    const successMsgPart = '\nFile has been read!';
+
+    console.log('---\tfile\tstart\t---')
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('end', () => {
+        console.log('\n---\tfile\tend\t---')
+        console.log(successMsgPart)
+    });
+    
+
+    readStream.on('error', (error) => {
+        console.error(errorMsgPart,error.message);
+    });
 };
 
 await read();
